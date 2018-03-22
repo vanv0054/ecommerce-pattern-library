121f30aa3d6ea4c06098e5b0727edbbc44ad4ae0
vanv0054 <vanv0054@algoqnuinlive.com>
2018-03-22 10:27:25 -0400

    Fix indentation issues

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/thegermandutchman/Desktop/Semester 4/Web Dev 4/ecommerce-pattern-library
- * @version 1521728515705
+ * @version 1521728834334
  */
-const patternManifest_1521728515705 = {
+const patternManifest_1521728834334 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -907,5 +907,5 @@ const patternManifest_1521728515705 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521728515705);
+patternBotIncludes(patternManifest_1521728834334);
 }());
