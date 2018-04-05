3f8105d780c95c857b2ae38a181c1b9bbd1d342b
vanv0054 <vanv0054@algoqnuinlive.com>
2018-04-05 08:22:05 -0400

    Make your order section line up with checkout form

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/thegermandutchman/Desktop/Semester 4/Web Dev 4/ecommerce-pattern-library
- * @version 1522334544323
+ * @version 1522930885443
  */
-const patternManifest_1522334544323 = {
+const patternManifest_1522930885443 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -912,5 +912,5 @@ const patternManifest_1522334544323 = {
   }
 };
 
-patternBotIncludes(patternManifest_1522334544323);
+patternBotIncludes(patternManifest_1522930885443);
 }());
